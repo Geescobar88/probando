eba0023e63ec28d77ede545807c1bdd4f0f71cd6
Gaston Escobar <gastonescobar1988@gmail.com>
2023-02-04 11:52:17 -0300

    cambios

--- Ajedrez/script.js
+++ Ajedrez/script.js
@@ -12,9 +12,3 @@ function drop(ev) {
   ev.target.appendChild(document.getElementById(data));
 }
 
-var x = document.getElementById("prueba");
-if (x.children.length > 0) {
-  console.log("El elemento tiene hijos.")
-} else {
-  console.log("El elemento no tiene hijos.")
-}
