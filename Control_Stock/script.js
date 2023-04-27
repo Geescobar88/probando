b25a3db8d252364775247da1bf45d9f51c8bec53
Gaston Escobar <gastonescobar1988@gmail.com>
2023-04-27 20:37:18 -0300

    Leer xls

--- Control_Stock/script.js
+++ Control_Stock/script.js
@@ -1,24 +1,52 @@
 var articulos;
 
-window.addEventListener('load', async function CargarData() {
-    const response = await fetch('https://geescobar88.github.io/probando/Control_Stock/data/Stock.json')
-    const data = await response.json()
-    articulos = data
-    for (i = 0; i < articulos.length; i++) {
+// window.addEventListener('load', async function CargarData() {
+//     const response = await fetch('https://geescobar88.github.io/probando/Control_Stock/data/Stock.json')
+//     const data = await response.json()
+//     articulos = data
+//     for (i = 0; i < articulos.length; i++) {
 
-        if (articulos[i].DESCRIPCION != "ANULAR") {
-            const newOption = document.createElement("option");
-            const lista = document.getElementById('medicacion');
-            const atribId = document.createAttribute("id");
-            atribId.value = "option" + i;
-            const atribValue = document.createAttribute("value");
-            atribValue.value = articulos[i].DESCRIPCION;
-            newOption.setAttributeNode(atribId);
-            newOption.setAttributeNode(atribValue);
-            lista.appendChild(newOption);
+//         if (articulos[i].DESCRIPCION != "ANULAR") {
+//             const newOption = document.createElement("option");
+//             const lista = document.getElementById('medicacion');
+//             const atribId = document.createAttribute("id");
+//             atribId.value = "option" + i;
+//             const atribValue = document.createAttribute("value");
+//             atribValue.value = articulos[i].DESCRIPCION;
+//             newOption.setAttributeNode(atribId);
+//             newOption.setAttributeNode(atribValue);
+//             lista.appendChild(newOption);
+//         }
+//     }
+// });
+
+window.addEventListener('load', async function CargarData() {
+    const response = await fetch('https://geescobar88.github.io/probando/Control_Stock/data/stock.xls')
+    const data = await response.arrayBuffer();
+    const workbook = XLSX.read(data, {type: 'array'});
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const range = XLSX.utils.decode_range(worksheet['!ref']);
+    const startRow = 2; 
+    const articulos = [];
+    for (let rowNum = startRow; rowNum <= range.e.r; rowNum++) {
+        const descripcion = worksheet[XLSX.utils.encode_cell({r: rowNum, c: 1})].v;
+        if (descripcion !== "ANULAR") {
+            articulos.push({descripcion: descripcion});
         }
     }
+    const lista = document.getElementById('medicacion');
+    for (let i = 0; i < articulos.length; i++) {
+        const newOption = document.createElement("option");
+        const atribId = document.createAttribute("id");
+        atribId.value = "option" + i;
+        const atribValue = document.createAttribute("value");
+        atribValue.value = articulos[i].descripcion;
+        newOption.setAttributeNode(atribId);
+        newOption.setAttributeNode(atribValue);
+        lista.appendChild(newOption);
+    }
 });
+
 
 function elegir() {
     let articulo = document.getElementById('listaArt').value;
---
+++ prueba/index.js
@@ -0,0 +1,12 @@
+opcion1 = document.getElementById("elegir_rojo");
+opcion2 = document.getElementById("elegir_verde");
+
+function elegir() {
+    if (opcion1.checked = true) {
+        document.body.style.backgroundColor = "rojo";
+    };
+
+    if (opcion2.checked = true) {
+        document.body.style.backgroundColor = "verde";
+    };
+}
