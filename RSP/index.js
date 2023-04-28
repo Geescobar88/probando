9c2a6ee61dbaa7d171d58144a9d0f2cb8c9e4d11
Gaston Escobar <gastonescobar1988@gmail.com>
2023-04-28 20:25:35 -0300

    RSP funcional y DB actualizada.

--- RSP/index.js
+++ RSP/index.js
@@ -1,11 +1,23 @@
-async function buscar() {
-    const response = await fetch('./DB_P.json')
+var documento;
+
+window.addEventListener('load', async function cargar() {
+    const response = await fetch('https://geescobar88.github.io/probando/RSP/DB_P.json')
     const data = await response.json()
-    console.log(data)
+    documento = data
+})
 
+function buscar() {
     const dni = document.getElementById('dni').value
+    const fin_resapro = document.getElementById('resapro');
 
- for (let i = 0; i < data.length ; i++ ) {
-            console.log(data[i].NRO_DOC)
-}
+    for (let i = 0; i < documento.length; i++) {
+        if (dni == documento[i].NRO_DOC && i <= (documento.length - 1)) {
+            fin_resapro.innerHTML = `Posee PROEPI activo hasta: ${documento[i].FECHA_FIN}`;
+            break
+        } else {
+            if (i == (documento.length - 1)) {
+                fin_resapro.innerHTML = "El paciente no esta inscripto en RESAPRO."
+            }
+        }
+    }
 }
