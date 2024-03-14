9595afdc05d3a36a596eda70a58fd41bc160dfc2
Gaston Escobar <gastonescobar1988@gmail.com>
2024-03-14 08:43:39 -0300

    DB actualizada

--- Etiquetas_Onco/index.js
+++ Etiquetas_Onco/index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  let dbResponse
+  setTimeout(() => {
+  fetch('./data/DB_MED.json')
+  .then(response => response.json())
+  .then(data => dbResponse = data);
+  console.log(dbResponse)
+}, "500");
+});
 
-  });
+const btnAgregar = document.getElementById("btnAgregar")
+btnAgregar.addEventListener('click', () => {
+
+})
