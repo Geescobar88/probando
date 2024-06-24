fc42d20eb2cbaaaa19e54ccceeae2392027e618e
Gaston Escobar <gastonescobar1988@gmail.com>
2024-06-24 13:33:27 -0300

    Etiquetas cambio

--- Control_Stock/etiquetas.js
+++ Control_Stock/etiquetas.js
@@ -11,24 +11,20 @@ const marca = document.getElementById("marca")
 const contenedor = document.getElementById("contenedor")
 const hoja = document.getElementById("hoja")
 const boton = document.getElementById("establecer")
+const volver_btn = document.getElementById("volver")
 
 boton.addEventListener("click", () => {
 header.style.display ="none"
-
 document.body.style.alignItems ="normal"
 
-
 const medicacion_etiq = document.querySelectorAll(".medicacion_etiq")
-
 const dosis_etiq = document.querySelectorAll(".dosis_etiq")
 const lote_etiq = document.querySelectorAll(".lote_etiq")
 const vencimiento_etiq = document.querySelectorAll(".vencimiento_etiq")
 const marca_etiq = document.querySelectorAll(".marca_etiq")
+
 for (i = 0; i < medicacion_etiq.length ; i++) {
     medicacion_etiq[i].textContent = medicacion.value
-    // if (medicacion.value.length > 12) {
-    //     medicacion_etiq[i].style.fontSize = "11px"
-    // }
     dosis_etiq[i].textContent = dosis.value
     lote_etiq[i].textContent = lote.value
     vencimiento_etiq[i].textContent = vencimiento.value
@@ -37,6 +33,12 @@ for (i = 0; i < medicacion_etiq.length ; i++) {
 
 contenedor.style.display ="none"
 hoja.style.display ="inline"
+volver_btn.style.display = "inline"
+})
+
+
+volver_btn.addEventListener("click", () => {
+  location.reload()
 })
   });
 
@@ -53,6 +55,7 @@ function menubar() {
     menu_close.addEventListener("click", () => {
       menu.style.display = "none"
     })
+
+
   
   }
-
