442268c329b9579eb6618835a8d4060330e4ab1f
Gaston Escobar <gastonescobar1988@gmail.com>
2024-09-12 09:12:03 -0300

    DB actualizada

--- Control_Stock/index.js
+++ Control_Stock/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const fechaSpan = document.getElementById("fecha")
   const fecha = new Date();
-  const diaActual = "10-9-2024"
+  const diaActual = "12-9-2024"
   fechaSpan.innerText = diaActual
   // const diaActual = fecha.getDate() + "-" + (fecha.getMonth() + 1) + "-" + fecha.getFullYear();
-  const diaPrevio = "09-9-2024"
+  const diaPrevio = "10-9-2024"
   cargarDatos(diaActual, diaPrevio);
 });
 
