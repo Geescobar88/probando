5b78f95cffb5c8acbb87f0432d19273e72913001
Gaston Escobar <gastonescobar1988@gmail.com>
2025-03-26 09:34:28 -0300

    DB actualizada

--- Control_Stock/index.js
+++ Control_Stock/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const fechaSpan = document.getElementById("fecha")
   const fecha = new Date();
-  const diaActual = "21-03-2025"
+  const diaActual = "26-03-2025"
   fechaSpan.innerText = diaActual
 
-  const diaPrevio = "20-03-2025"
+  const diaPrevio = "21-03-2025"
   cargarDatos(diaActual, diaPrevio);
 
   const arregloDia = () => {
