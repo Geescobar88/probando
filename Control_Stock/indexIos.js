d059b132048d7934a1ff1bcadc1d2b34c2620d63
geescobar <gastonescobar1988@gmail.com>
2025-08-12 15:46:18 -0300

    DB actualizada

--- Control_Stock/indexIos.js
+++ Control_Stock/indexIos.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const fechaSpan = document.getElementById("fecha")
   const fecha = new Date();
-  const diaActual = "11-08-2025"
+  const diaActual = "12-08-2025"
   fechaSpan.innerText = diaActual
 
-  const diaPrevio = "06-08-2025"
+  const diaPrevio = "11-08-2025"
   cargarDatos(diaActual, diaPrevio);
 
   const arregloDia = () => {
